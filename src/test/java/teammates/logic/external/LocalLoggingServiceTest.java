edd3a2ac2a1b30c6ab20612f3db820c16dae8ba8
Benjaneb <benjaneb@proton.me>
2026-02-19 02:04:08 +0100

    Add test attempting to cover isRequestFilterSatisfied

---
+++ src/test/java/teammates/logic/external/LocalLoggingServiceTest.java
@@ -0,0 +1,28 @@
+package teammates.logic.external;
+
+import java.time.Instant;
+
+import org.testng.annotations.Test;
+
+import teammates.common.datatransfer.QueryLogsResults;
+import teammates.common.datatransfer.logs.QueryLogsParams;
+import teammates.test.BaseTestCase;
+
+/**
+ * SUT: {@link LocalLoggingService}.
+ */
+public class LocalLoggingServiceTest extends BaseTestCase {
+
+    private final LocalLoggingService llogService = new LocalLoggingService();
+
+    @Test
+    public void testDefaultLogsHaveNextPage() {
+        llogService.createFeedbackSessionLog("dd2480", "teacher@kth.se", "test", "test");
+        long currentTimestamp = Instant.now().toEpochMilli();
+        QueryLogsParams params = QueryLogsParams.builder(0, currentTimestamp)
+                .build();
+        QueryLogsResults res = llogService.queryLogs(params);
+
+        assertTrue(res.getHasNextPage());
+    }
+}
